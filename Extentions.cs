38fee9c3d77f6cf5be135a24f299eb145905863c
AhmedSelim04 <ahmed.selim.fci@gmail.com>
2025-02-25 05:06:14 +0200

    Add New version

---
+++ Extentions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracker
+{
+    public static class Extensions 
+    {
+        public static string[] CLSplit(this string? input)//Command Line Split
+        {
+            if (input.Length==0)
+            {
+                string[] x = { "" };
+                return x;
+            }
+            string[] inputs = input.Split(' ');
+            List<string> list=new List<string>();
+            bool inName = false;
+            for(int i = 0; i < inputs.Length; i ++)
+            {
+                if (inputs[i][0] == '"') inName = true;
+                if (inName)
+                {
+                    string x="";
+
+                    while (i < inputs.Length && inputs[i][^1] != '"') 
+                    {
+                        x += inputs[i++];
+                    }
+                    x += inputs[i];
+                    inName = false;
+                    list.Add(x);
+                }
+                else
+                {
+                    list.Add(inputs[i]);
+                }
+            }
+            return list.ToArray();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.Json;
+using TaskTracker;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            string[] x = Console.ReadLine().CLSplit();
+            
+            
+        }
+    }
+    
+}
